7194fde0fb28ae497e3611ef6d255a52b7bdaf8a
marianom23 <81484301+marianom23@users.noreply.github.com>
2021-10-10 17:17:10 -0300

    Delete Parcial 1 - Programacion 3 directory

--- Parcial 1 - Programacion 3/src/main/java/com/example/testing/repositories/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.testing.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseRepository<E, Long> extends JpaRepository<E, Long> {
-}
--- Parcial 1 - Programacion 3/src/main/java/com/example/testing/repositories/LibroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.testing.repositories;
-
-import com.example.testing.entities.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibroRepository extends BaseRepository<Libro, Long> {
-}
